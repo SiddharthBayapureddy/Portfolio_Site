37aed0879b5f69ad0abc5ec767f349f078e89ddb
Siddharth <siddharthbayapureddy@gmail.com>
2025-11-14 20:05:08 +0530

    Added contact me page and background texture

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 import json
 
-# Loading portfolio data
 def load_portfolio_data():
 
+    # Loadinf json data
     try:
-        with open("portfolio_data.json" , "r") as file:
+        with open("portfolio_data.json", "r") as file:
             data = json.load(file)
         return data
     except FileNotFoundError:
-        print("ERROR: File not found")
+        print("ERROR: File 'portfolio_data.json' not found.")
         return {}
     except json.JSONDecodeError:
-        print("ERROR: Could not decode file")
+        print("ERROR: Could not decode JSON from 'portfolio_data.json'.")
         return {}
 
-# Flash application
+# Flask application
 app = Flask("Portfolio")
 
-
-# Loading data from json
+# Loading all data from JSON
 portfolio_data = load_portfolio_data()
-
-about_me = portfolio_data.get("about_me" , {})
+about_me = portfolio_data.get("about_me", {})
 social_media = portfolio_data.get('social_media', [])
 experiences = portfolio_data.get('experiences', [])
 projects = portfolio_data.get('projects', [])
 
-
 @app.context_processor
-def inject_socials():
+def inject_global_data():
     return dict(social_media=social_media)
 
-
-# Root Directory
-@app.route('/' , strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def index():
-    # Returns about me section
-    return render_template("index.html" , about=about_me)
+    return render_template(
+        "index.html",
+        about=about_me,
+        projects=projects,
+        experiences=experiences
+    )
 
+@app.route('/contact', strict_slashes=False)
+def contact():
+    return render_template("contact.html")
 
-# Projects page
-@app.route("/projects")
-def projects_page():
-    # Returns my projects
-    return render_template("projects.html" , projects=projects)
-
-
-# Experiences Page
-@app.route('/experience')
-def experience_page():
-    """Serves the experience page."""
-    return render_template('experiences.html', experiences=experiences)
-
-
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
